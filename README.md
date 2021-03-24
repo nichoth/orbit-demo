579be37b0966db8e1d1e4662687e7ea823ac31fc
nichoth <nichoth@gmail.com>
2021-03-23 23:30:48 -0700

    working

--- README.md
+++ README.md
@@ -21,6 +21,15 @@ Thwarted by errors
 Error: No resolver found for codec "undefined"
 ```
 
+------------------------------------------------------
+
+## 3-22-2021
+
+* https://medium.com/coinmonks/orbitdb-a-peer-to-peer-database-for-the-decentralized-web-30bac1d056fe
+* https://github.com/orbitdb/orbit-db
+* https://github.com/orbitdb/orbit-db/blob/master/API.md#orbitdbdocsnameaddress-options
+* https://github.com/orbitdb/orbit-db/blob/master/GUIDE.md
+* https://github.com/orbitdb/orbit-db-docstore
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,60 +1,89 @@
 const IPFS = require('ipfs')
 const OrbitDB = require('orbit-db')
+var timestamp = require('monotonic-timestamp')
 
+// https://github.com/orbitdb/orbit-db#module-with-ipfs-instance
 async function main () {
     const initIPFSInstance = async () => {
-        return await IPFS.create({ repo: "./path-for-js-ipfs-repo" })
+        return await IPFS.create({
+            pubsub: true,
+            EXPERIMENTAL: { pubsub: true },
+            repo: "./path-for-js-ipfs-repo"
+        })
     }
 
     initIPFSInstance().then(async ipfs => {
         const orbitdb = await OrbitDB.createInstance(ipfs)
-    
+
         // Create / Open a database
-        const db = await orbitdb.log("hello")
+        var db = await orbitdb.log('test')
         await db.load()
     
         // Listen for updates from peers
         db.events.on("replicated", address => {
-            console.log('*replicaated*', address,
+            console.log('*replicated*')
+            console.log('*replicated*', address,
                 db.iterator({ limit: -1 }).collect())
         })
     
         // Add an entry
-        const hash = await db.add("world")
+        const hash = await db.add({
+            _id: 'QmAwesomeIpfsHash',
+            name: 'foo',
+            followers: 500
+        })
+        // const hash = await db.add("world")
         console.log('*hash*', hash)
     
         // Query
-        const result = db.iterator({ limit: -1 }).collect()
-        console.log('*query*', JSON.stringify(result, null, 4))
+        // const result = db.query(doc => (doc))
+        // const result = db.iterator({ limit: -1 }).collect()
+        // console.log('*query*', JSON.stringify(result, null, 4))
+
+        replication(db)
     })
+
+    async function replication (db) {
+        // Create the second peer
+        console.log('in replication****')
+        const ipfs2_config = { repo: './ipfs2', }
+        const ipfs2 = await IPFS.create(ipfs2_config)
+
+        // Open the first database for the second peer,
+        // ie. replicate the database
+        const orbitdb2 = await OrbitDB.createInstance(ipfs2, {
+            directory: './orbitdb2'
+        })
+        console.log('**addr**', db.address.toString())
+        // tried changin this to `open`
+        const db2 = await orbitdb2.open(db.address.toString())
+        await db2.load()
+
+        console.log('Making db2 check replica')
+
+        db.events.on('replicated', (ev) => {
+            console.log('aaaaaaaa', ev)
+        })
+
+
+        // When the second database replicated new heads, query the database
+        db2.events.on('replicated', () => {
+            console.log('**replicated**')
+            const result = db2.iterator({ limit: -1 })
+                .collect().map(ev => ev.payload.value)
+
+            console.log('**result**', result.join('\n'))
+        })
+
+        // Start adding entries to the first database
+        setInterval(async () => {
+            console.log('**interval**')
+            await db.add({ _id: timestamp(), time: timestamp() })
+        }, 2000)
+    }
+
 }
 
 main()
 
-
-
-// -------------------------------------------------------------------
-
-
-// https://github.com/orbitdb/field-manual/blob/master/01_Tutorial/01_Basics.md
-
-// // const Ipfs = require('ipfs')
-// // const OrbitDB = require('orbit-db')
-// // var Npp = require('./new-piece-please')
-
-// var npp = require('./new-piece-please')
-
-// // var npp = new Npp(Ipfs, OrbitDB)
-
-// npp.onready = async () => {
-//     // console.log('**id**', npp.orbitdb.id)
-//     console.log('**pieces**', npp.pieces.id)
-
-//     const cid = npp.addNewPiece('QmNR2n4zywCV61MeMLB6JwPueAPqheqpfiA4fLPMxouEmQ')
-//     const content = await npp.node.dag.get(cid)
-//     console.log('node.dag.get', content.value.payload)
-
-//     console.log('fooooo')
-// }
-
-// console.log('hello')
+console.log('hello')
