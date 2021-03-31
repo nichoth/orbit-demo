0cbd1fafeb3e24a61936a5ae18be1fd8577b542e
nichoth <nichoth@gmail.com>
2021-03-30 17:31:33 -0700

    trying things

--- src/index.js
+++ src/index.js
@@ -2,6 +2,35 @@ const IPFS = require('ipfs')
 const OrbitDB = require('orbit-db')
 var timestamp = require('monotonic-timestamp')
 
+
+
+// -----------------------------------------------
+
+const defaultFilter = () => true
+
+const connectIpfsNodes = async (ipfs1, ipfs2, options = {
+  filter: defaultFilter
+}) => {
+    const id1 = await ipfs1.id()
+    const id2 = await ipfs2.id()
+
+    console.log('**ids**', id1, id2)
+
+    const addresses1 = id1.addresses.filter(options.filter)
+    const addresses2 = id2.addresses.filter(options.filter)
+
+    console.log('in here addr***', addresses1, addresses2)
+
+    await ipfs1.swarm.connect(addresses2[0])
+    await ipfs2.swarm.connect(addresses1[0])
+}
+
+
+// -----------------------------------------------
+
+
+
+
 // https://github.com/orbitdb/orbit-db#module-with-ipfs-instance
 async function main () {
     const initIPFSInstance = async () => {
@@ -15,15 +44,18 @@ async function main () {
     initIPFSInstance().then(async ipfs => {
         const orbitdb = await OrbitDB.createInstance(ipfs)
 
+        ipfs.libp2p.on('peer:connect', (peer) => {
+            console.info('Producer Found:', peer.id)
+        })
+
         // Create / Open a database
         var db = await orbitdb.log('test')
         await db.load()
     
         // Listen for updates from peers
         db.events.on("replicated", address => {
-            console.log('*replicated*')
-            console.log('*replicated*', address,
-                db.iterator({ limit: -1 }).collect())
+            console.log('*replicated*', address)
+            console.log(db.iterator({ limit: -1 }).collect())
         })
     
         // Add an entry
@@ -40,31 +72,62 @@ async function main () {
         // const result = db.iterator({ limit: -1 }).collect()
         // console.log('*query*', JSON.stringify(result, null, 4))
 
-        replication(db)
+        replication(db, ipfs)
     })
 
-    async function replication (db) {
+    async function replication (db, ipfs) {
         // Create the second peer
         console.log('in replication****')
-        const ipfs2_config = { repo: './ipfs2', }
+        const ipfs2_config = {
+            repo: './ipfs2',
+            pubsub: true,
+            EXPERIMENTAL: { pubsub: true },
+            Bootstrap: [db.address.toString()]
+        }
         const ipfs2 = await IPFS.create(ipfs2_config)
+
+        connectIpfsNodes(ipfs, ipfs2)
 
         // Open the first database for the second peer,
         // ie. replicate the database
         const orbitdb2 = await OrbitDB.createInstance(ipfs2, {
             directory: './orbitdb2'
         })
+        const db2 = await orbitdb2.log(db.address.toString());
+        // const db2 = await orbitdb2.open(db.address.toString())
+
         console.log('**addr**', db.address.toString())
-        // tried changin this to `open`
-        const db2 = await orbitdb2.open(db.address.toString())
+        // tried changing this to `open`
+        // const db2 = await orbitdb2.open(db.address.toString())
         await db2.load()
+
+
+
+
+        // var protocol = '/p2p-circuit/ipfs'
+        // var multiaddr = db.address.toString()
+        // console.log('swarm.connect: ', protocol + multiaddr) 
+        // console.log('multiaddr', multiaddr) 
+
+        // await ipfs2.swarm.connect(multiaddr, {
+        //     recursive: false
+        // })
+
+
+
+
+
+        ipfs2.libp2p.on('peer:connect', (peer) => {
+            console.info('Producer Found:', peer.id)
+        })
+
+
 
         console.log('Making db2 check replica')
 
         db.events.on('replicated', (ev) => {
-            console.log('aaaaaaaa', ev)
+            console.log('original db replicated', ev)
         })
-
 
         // When the second database replicated new heads, query the database
         db2.events.on('replicated', () => {
@@ -79,6 +142,7 @@ async function main () {
         setInterval(async () => {
             console.log('**interval**')
             await db.add({ _id: timestamp(), time: timestamp() })
+            // await db2.add({ _id: timestamp(), time: timestamp() })
         }, 2000)
     }
 
